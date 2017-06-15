924eb004f0d70de6973b80e04aa030a2380a1d5e
Kamal <kamal@mtxb2b.com>
2017-06-15 18:03:30 +0530

    Add Location Screen Integrate ESRI Map Update Chart UI Add Bar Chart

---
+++ Knock/CustomUI/LocationCustomViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  LocationCustomViewCell.swift
+//  MTXGIS
+//
+//  Created by Kamal on 23/02/17.
+//  Copyright © 2017 mtxb2b. All rights reserved.
+//
+
+import UIKit
+
+class LocationCustomViewCell: UITableViewCell {
+
+    @IBOutlet weak var dataLocation: UILabel!
+    @IBOutlet weak var dataFullAddress: UILabel!
+    @IBOutlet weak var dataLocId: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
